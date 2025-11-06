139ab01043addaf33fe57d280df7d064b2b6ca1e
LucasL32323 <lucaslimamotta2060@hotmail.com>
2025-11-05 21:56:22 -0300

    Add files via upload

---
+++ Gamemania/carrinho.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoeaded' , () =>{
+Document.querrySelectorAll('.add-to-cart').forEach(button => {
+button.addEventListener('click' , (event) =>{
+        event.preventDefault();
+        alert('Produto adicionado com sucesso');
+     });
+   });
+});
+
+function validateEmail(email)
+{
+
+
+const re =
+ /^^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+return re.test(String(email),toLowerCase());
+const newsletterForm = document.querrySelector('newsletter-form');
+if (newsletterForm)
+{
+   newsletterForm.addEventListener('submit' ,(event)=> {
+      event.preventDefault();
+      const emailInput = newsletterForm.querrySelector();
+   })
+}
+}
+
+
+
